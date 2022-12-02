5199296a88879f42fa2fe801a15fa38a5ced899e
JhonM <jhon@example.com>
2022-12-02 15:09:44 -0500

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import {useState, useEffect} from "react";
+import { Container, Typography } from "@mui/material";
+
+const Home = () => {
+    return(
+        <Container sx={{ backgroundColor: '' }}>
+            <Typography variant="h4" align="left">Pagina Principal :3</Typography>
+        </Container>
+    );
+};
+
+export default Home;
